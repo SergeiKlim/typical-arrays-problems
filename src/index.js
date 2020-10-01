724e9f3b41d53d0bb5fa90dee9194cb501ce6278
SergeiKlim <70857184+SergeiKlim@users.noreply.github.com>
2020-10-01 20:23:37 +0300

    init: start Typical Arrays Problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-
 exports.min = function min (array) {
-  return 0;
+
+  if(!array || array.length === 0) {
+    return 0;
+  }
+  let minimum = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < minimum) {
+            minimum = array[i];
+        }
+    }
+    return minimum;
+    console.log(minimum)
+
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length === 0) {
+    return 0;
+  }
+    let maximum = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > maximum) {
+          maximum = array[i];
+        }
+    }
+    return maximum;
+    console.log(maximum)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  let average = 0;
+  if(!array || array.length === 0) {
+    return 0;
+  }
+    for (let i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+    }
+    average = sum / array.length;
+    return average;
+    console.log(average);
 }
